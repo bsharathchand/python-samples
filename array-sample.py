9ce364782832d77c1229cd0aed1c6607ce78e867
Bhaskara, Sharath <bsharathchand@gmail.com>
2017-07-23 18:16:07 -0700

    array-sample issue fixed

---
+++ array-sample.py
@@ -0,0 +1,12 @@
+def addarrays(array1,array2):
+    """ adds arrays and returns a single list """
+    temp = array1.append (array2)
+    print (temp)
+    temp
+
+if __name__ == "__main__":
+    integers=[1,2,3,4,5,6]
+    chars = ['a','b','c','d','e']
+    addarrays (integers,chars)
+
+
